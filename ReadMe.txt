0440d631236cd69a8e3655e89098165435469635
SHRIHARSHAMADNAL <shriharsha.madnal@gmail.com>
2023-05-21 14:53:44 +0530

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+1st step - npm install
+2nd Step - npm start
+
+
+If Doesnt work - Please install ts-node globally and run ts-node toDoList.ts
---
+++ toDoList.ts
@@ -0,0 +1,60 @@
+class Task {
+    constructor(
+      public name: string,
+      public description: string,
+      public dueDate: Date,
+      public status: string
+    ) {}
+  
+    updateStatus(newStatus: string): void {
+      this.status = newStatus;
+    }
+  }
+  
+  class TodoList {
+    private tasks: Task[];
+  
+    constructor(public name: string, public description: string) {
+      this.tasks = [];
+    }
+  
+    addTask(task: Task): void {
+      this.tasks.push(task);
+    }
+  
+    deleteTask(task: Task): void {
+      const index = this.tasks.indexOf(task);
+      if (index !== -1) {
+        this.tasks.splice(index, 1);
+      }
+    }
+  
+    editTask(task: Task, newName: string, newDescription: string, newDueDate: Date): void {
+      task.name = newName;
+      task.description = newDescription;
+      task.dueDate = newDueDate;
+    }
+  
+    getTasks(): Task[] {
+      return this.tasks;
+    }
+  }
+
+const task1 = new Task('Task 1', 'read book', new Date(2023, 5, 21), 'not started');
+const task2 = new Task('Task 2', 'Apply for Exam', new Date(2023, 5, 22), 'in progress');
+const task3 = new Task('Task 3', 'book Match Tickets', new Date(2023, 5, 23), 'not started');
+
+const todoList1 = new TodoList('personal To Do List', 'My personal tasks');
+todoList1.addTask(task1);
+todoList1.addTask(task2);
+
+const todoList2 = new TodoList('Tournament To Do list', 'Tasks for Cricket Tournament');
+todoList2.addTask(task3);
+
+console.log(todoList1.getTasks(),"New list1");
+console.log(todoList2.getTasks(),"New List 2")
+
+todoList2.deleteTask(task3)
+todoList1.deleteTask(task1)
+console.log(todoList1.getTasks(),"List 1 With Deleted Task")
+console.log(todoList2.getTasks(),"List 2 With Deleted Task")
